22ffc805408e784d415218088fa24befaf6cf5da
Ahmed10257 <ahmed.mansour10257@gmail.com>
2024-06-09 02:46:46 +0300

    inital commit

--- Back-End/src/app.module.ts
+++ Back-End/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DoctorModule } from './doctor/doctor.module';
+import { PatientModule } from './patient/patient.module';
+import { AppointmentModule } from './appointment/appointment.module';
+import { ConsultationModule } from './consultation/consultation.module';
 
 @Module({
-  imports: [],
+  imports: [DoctorModule, PatientModule, AppointmentModule, ConsultationModule],
   controllers: [AppController],
   providers: [AppService],
 })
