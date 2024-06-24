986147fa1515f5f05fd1a54e73191c24221d6898
Mohamed Torkey <120189395+mohamedtorkey20@users.noreply.github.com>
2024-06-24 13:54:29 +0300

    add the components of desgin and add some properties to doctor schema

--- Front-End/src/pages/DoctorProfile.tsx
+++ Front-End/src/pages/DoctorProfile.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import SearchBar from "../components/doctorProfile/SearchBar";
 import DoctorInfo from "../components/doctorProfile/DoctorInfo";
 import DoctorAbout from "../components/doctorProfile/DoctorAbout";
@@ -6,44 +6,63 @@ import PatientSatisfaction from "../components/doctorProfile/PatientSatisfaction
 import OverallRating from "../components/doctorProfile/OverallRating";
 import PatientRate from "../components/doctorProfile/PatientRate";
 import BookingDoctorProfile from "../components/doctorProfile/BookingDoctorProfile";
-import {useDoctorData,useReviewsOfDoctor} from "../hooks/useDoctorData";
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import { IDoctor } from "../interfaces";
 
 interface IProps {}
 
 const DoctorProfile = (props: IProps) => {
     console.log(props);
+    
+    const [dataDoctor, setDataDoctor] = useState<IDoctor>({
+    name: '',
+  phone: -1,
+  email: '',
+  password: '',
+  about: '',
+  address: {
+    city: '',
+    country: '',
+    region: -1
+  },
+  birthdate: '',
+  fees: -1,
+  genaralSpecialization: '',
+  gender: '',
+  image: '',
+  isDoctor: true,
+  specializes: [],
+  views: 1000
+    });
+    const { id } = useParams();
+    
+    useEffect(() => {
+        const fetchDoctorData = async () => {
+            try {
+                const response = await axios.get(`http://127.0.0.1:3000/doctor/${id}`);
+                setDataDoctor(response.data);
+            } catch (error) {
+                console.error("Error fetching doctor data:", error);
+            }
+        };
 
-    const { dataDoctor, loading, error } = useDoctorData();
-    const {reviews,overallRating }=useReviewsOfDoctor();
+        fetchDoctorData();
+    }, [id]);
 
-    if (loading) {
+    
+
+    
+        console.log(dataDoctor);
+        
+    if (!dataDoctor) {
         return <div>Loading...</div>;
     }
 
-    if (error) {
-        return <div>{error}</div>;
-    }
-
-    if (!dataDoctor) {
-        return <div>No data available</div>;
-    }
-
-    console.log(overallRating);
-    
-    //render
-    const renderReviews = reviews.map((review, indx) => (
-        <PatientRate
-            key={indx}
-            rating={review.rating}
-            dateReview={new Date(review.createdAt).toLocaleDateString()}
-            namePatinet={review.patient.name}
-            reviewComment={review.review}
-        />
-    ));
     return (
         <>
             <div className="hidden md:block">
-                <SearchBar doctorName={dataDoctor.name} phoneNubmer={dataDoctor.phone} />
+                <SearchBar doctorName={dataDoctor.name}  phoneNubmer={dataDoctor.phone} />
             </div>
             <div className="w-full h-full bg-gray-200 flex flex-col justify-start items-start pt-3">
                 <div className="px-16 hidden lg:block">
@@ -57,18 +76,20 @@ const DoctorProfile = (props: IProps) => {
                 <div className="w-full flex justify-center items-start px-16">
                     <div className="">
                         <DoctorInfo doctorName={dataDoctor.name} view={dataDoctor.views} 
-                            genaralSpecialization={dataDoctor.genaralSpecialization} 
-                            specializes={dataDoctor.specializes} imageProfile={dataDoctor.image}
-                            numberOfReviews={reviews.length} overallReview={overallRating}
+                        genaralSpecialization={dataDoctor.genaralSpecialization} 
+                        specializes={dataDoctor.specializes} imageProfile={dataDoctor.image}
                         />
                         <div className="lg:hidden">
                             <BookingDoctorProfile  />
                         </div>
                         <DoctorAbout about={dataDoctor.about} />
                         <PatientSatisfaction />
-                        <OverallRating  overallRating={overallRating}  />
+                        <OverallRating />
                         <div>
-                          {renderReviews}
+                            <PatientRate />
+                            <PatientRate />
+                            <PatientRate />
+                            <PatientRate />
                         </div>
                     </div>
                     <div className="w-full mx-4 hidden lg:block">
